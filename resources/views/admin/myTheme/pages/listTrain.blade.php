05ee5b0f0ffa22c4ce663aec75e9508207bbb513
rahul25062002 <mmgbrahul@gmail.com>
2024-04-04 19:22:16 +0530

    makeEditmodelToAppearOnClick

--- resources/views/admin/myTheme/pages/listTrain.blade.php
+++ resources/views/admin/myTheme/pages/listTrain.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.myTheme.layout')
 
 @section('listTrain')
-    <div class='pl-4 pr-4 flex justify-center border-0 flex-col ' ng-controller="getTrains">
+    <div class='pl-4 pr-4 flex justify-center  flex-col ' ng-controller="getTrains">
         <div class='w-[95%]'>
             <ul class='flex justify-between gap-y-6 bg-gray-400 border-solid border-b-4 border-gray-50 '>
                 <li class='bg-red-100 w-12  text-center'>s.no</li>
@@ -24,7 +24,7 @@
                     <li><% train.startTime %></li>
                     <li><% train.startTime %></li>
                     <li>
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" ng-click='edit(train.Train_no )'>Edit</button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" ng-click='edit( index )'>Edit</button>
                         <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" ng-click='delete( train.Train_no )'>Delete</button>
                     </li>
                     
@@ -34,43 +34,87 @@
 
 
            
-        <div class='bg-red-200 w-[60%] border border-black px-2 '>
-              <form action="" class='flex flex-col gap-4'>
+        <div class='bg-red-200 w-[96%] border border-black px-2 modalEdit absolute  top-[15%] hidden '>
+              <form action="" class='flex flex-col gap-4 items-center w-[80%]'>
                 <span>Edit the train</span>
-
-                <label for="">
-                    Train No.
-                    <input type="text">
-                </label>
-
-                <label for="">
-                    Start From
-                    <input type="text">
-                </label>
-
-                <label for="">
-                    End At
-                    <input type="text">
-                </label>
-
-                <label for="">
-                    Start Time
-                    <select name="" id="" class='appearance-none'>
-                        <option value="hr" ng-repeat='hr in h'><% hr %></option>
-                    </select>
-                    <select name="" id="" class='appearance-none'>
-                        <option value="min" ng-repeat='min in m'  ><% min %></option> 
-                    </select>
-                    <select name="" id="" >
-                        <option value="a" ng-repeat='a in ["AM","PM"]' ><% a %></option>
-                    </select>
-                </label>
-
-                <label for="">
-                    End Time
-                    <input type="time">
-                </label>
+                <% allTrain[particularTrain] %>
+                <div class='flex gap-[15%] border'>
+                       
+                    <label for="" class='w-[23%] '>
+                        Train No.:-
+                    </label>
+                    <div>
+    
+                        <input type="text" value='<% allTrain[particularTrain].Train_no %>' readonly>
+                    </div>
+                </div>
                 
+                <div class='flex gap-[15%] '>
+
+                    <label for=""  class='w-[23%] '>
+                        Start From :-
+                    </label>
+                    <div>
+    
+                        <input type="text" value='<% allTrain[particularTrain].stationFrom %>'>
+                    </div>
+                </div>
+
+                 <div class='flex gap-[15%] '>
+
+                     <label for=""  class='w-[23%] '>
+                         End At :-
+                     </label>
+                     <div>
+     
+                         <input type="text"  value='<% allTrain[particularTrain].stationTo %>'>
+                     </div>
+                 </div>
+                
+                 <div class='flex gap-[15%] '>
+
+                     <label for=""  class='w-[23%]  '>
+                         Start Time :-
+                     </label>
+                     <div >
+                        <% selectedStartTime[1]%>
+                        
+                         <select name="" id="" class='appearance-none w-8 text-center' ng-model=' selectedStartTime[0]'>
+                             <option  ng-repeat='hr in h' ><% hr %></option>
+                         </select>
+                         <select name="" id="" class='appearance-none w-8 text-center' ng-model='selectedStartTime[1]'>
+                             <option  ng-repeat='min in m'  ><% min %></option> 
+                            
+                         </select>
+                         <select name="" id=""  ng-model='selectedStartTime[2]'>
+                             <option value="a" ng-repeat='a in ["AM","PM"]' ><% a %></option>
+                         </select>
+                     </div>
+                 </div>
+               
+                 <div class='flex gap-[15%] '>
+
+                     <label for=""  class='w-[23%] '>
+                         End Time :-
+                     </label>
+                         <div>
+     
+                             <select name="" id="" class='appearance-none w-8 text-center'>
+                                 <option value="hr" ng-repeat='hr in h'><% hr %></option>
+                             </select>
+                             <select name="" id="" class='appearance-none w-8 text-center some'>
+                                 
+                                 <option value="min" ng-repeat='min in m'  ><% min %></option> 
+                                 
+                                 
+                             </select>
+                             <select name="" id="" >
+                                 <option value="a" ng-repeat='a in ["AM","PM"]' ><% a %></option>
+                             </select>
+                         </div>
+                 </div>
+               
+                 <button ng-click='closeMode' class='bg-red-500 w-16 rounded p-2'>Submit</button>
               </form>
     
         </div>
@@ -95,10 +139,32 @@
                 });
 
             };
+             $scope.particularTrain;
 
             $scope.edit=function ($id){
-                console.log($id);
+                let v=document.querySelector('.modalEdit').classList.remove('hidden');
+                $scope.particularTrain=$id;
+               $scope.selectedStartTime= $scope.handleTimefun( $scope.allTrain[ $scope.particularTrain].startTime);
+               $scope.selectedEndTime= $scope.handleTimefun( $scope.allTrain[ $scope.particularTrain].endTime);
+                console.log(typeof  $scope.selectedStartTime[0])
+                console.log(typeof $scope.h[0])
+                // console.log(  $scope.allTrain  );
             };
+
+         $scope.handleTimefun=function ($data){
+            let temp=$data.split(':');
+            let temp2=temp[1].split(' ');
+            
+            return  [temp[0] , temp2[0] , temp2[1]]; 
+         }
+
+            $scope.closeMode=function (){
+                document.querySelector('.modalEdit').classList.add('hidden');
+            }
+
+
+
+
             $scope.delete=function ($id){
                 console.log($id);
             };
@@ -107,8 +173,11 @@
             $scope.hours=function (){
                 let hour=[];
                 for(let i=1;i<=12;i++){
-
-                    hour.push(i.toString());
+                    let temp=i.toString();
+                    if(temp.length==1){
+                        temp='0'+temp;
+                    }
+                    hour.push(temp);
                 }
                  console.log(hour)
                 $scope.h= hour;
@@ -121,8 +190,11 @@
             $scope.mins=function (){
                 let min=[];
                 for(let i=1;i<=60;i++){
-                    
-                    min.push(i.toString());
+                    let temp=i.toString();
+                    if(temp.length==1){
+                        temp='0'+temp;
+                    }
+                    min.push(temp);
                 }
 
                 $scope.m=min;
